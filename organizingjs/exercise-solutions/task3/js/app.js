61f2ed18cb756f6a9688e94dc637721768ad80b6
Jelle Kralt <jelle@jellekralt.nl>
2016-06-24 15:10:00 +0200

    Removed organizingjs

--- organizingjs/exercise-solutions/task3/js/app.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	Header.init();
-	Carousel.init();
-	Details.init();
-});
--- organizingjs/exercise-solutions/task4/js/app.js
+++
@@ -1,9 +0,0 @@
-(function(global){
-
-	global.EVT = new EventEmitter2();
-
-	$(document).ready(function(){
-		EVT.emit("init");
-	});
-
-})(window);
